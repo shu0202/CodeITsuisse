7e573b9013718798b6a684a67bc31438d2316957
shu0202 <91327331+shu0202@users.noreply.github.com>
2021-09-25 19:33:57 +0800

    Create fixedrace.py

---
+++ codeitsuisse/routes/fixedrace.py
@@ -0,0 +1,20 @@
+import logging
+import json
+import random
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(name)
+
+@app.route('/fixedrace', methods=['POST'])
+def fixedrace():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    swimmer_list = data.split(",")
+    result = random.shuffle(swimmer_list)
+    return json.dumps(result)
+
+
+
