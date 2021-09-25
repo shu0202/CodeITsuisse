3948fe28af85f55a8eebb027330c42fc5bd5fed7
shu0202 <91327331+shu0202@users.noreply.github.com>
2021-09-25 12:07:01 +0800

    Create asteroid.py

---
+++ codeitsuisse/routes/asteroid.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/asteroid', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation []".format(data))
+    inputValue = data.get("input")
+    print ("inputValue")
+    logging.info("My result :{}".format(inputValue))
+    return json.dumps(inputValue)
+
+
+
