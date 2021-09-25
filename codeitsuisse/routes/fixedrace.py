154fbd4c5d659a9fb3ed0046c10766d51e6b86ab
shu0202 <91327331+shu0202@users.noreply.github.com>
2021-09-25 21:06:34 +0800

    Update fixedrace.py

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def fixedrace():
-    result = ''
-    return result
+    data = request.get_data(as_text=True)
+    return ("")
 
 
 
